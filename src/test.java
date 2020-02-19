35739fe2ec995f610b8664bfbb91d71cfa47f7fd
Danylo Halaiko <dan.galaiko@gmail.com>
2020-02-19 15:52:05 +0200

    I`ve add ne function.

--- src/test.java
+++ src/test.java
@@ -2,4 +2,18 @@ public class test {
     public static void main(String[] args) {
         System.out.print("Hello world!");
     }
+
+    public static dataInput[] createData(String[] participator) {
+        dataInput[] returning = new dataInput[participator.length];
+        for (int k = 0; k < participator.length; k++) {
+            String[] temp = participator[k].split(",");
+            returning[k].SetName(temp[0]);
+            int sum =0;
+            for (int j =1; j<temp.length;j++){
+                sum+=Integer.parseInt(temp[k]);
+            }
+            returning[k].SetScore(sum);
+        }
+        return returning;
+    }
 }
