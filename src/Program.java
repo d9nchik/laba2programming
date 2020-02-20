5489c889af1ff9c59a234aa6ce82af9dfc2ee4da
Danylo Halaiko <dan.galaiko@gmail.com>
2020-02-20 13:24:37 +0200

    Some changes

--- src/Program.java
+++ src/Program.java
@@ -5,8 +5,13 @@ public class Program {
 
 
     public static void main(String[] args) throws FileNotFoundException {
+        String[] participator = ReaderWriter.create_matrix_information("C:\\Users\\danga\\IdeaProjects\\laba2programming\\tests\\eurovision2.csv");
+        /*Contender[] returning = new Contender[participator.length];
+        for (int j = 0; j < participator.length; j++) {
+            returning[j] = new Contender();
+        }*/
 
-        Contender[] data = VotesProcessor.createData(ReaderWriter.create_matrix_information("C:\\Users\\danga\\IdeaProjects\\laba2programming\\tests\\eurovision2.csv"));
+        Contender[] data = VotesProcessor.createData(participator);
         System.out.println("=====================================");
         System.out.println("Вот все элементы которые были в начальном файле:");
         for (Contender datum : data) {
